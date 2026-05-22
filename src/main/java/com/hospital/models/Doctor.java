645c6b51ae81454b3e18d7578eb52cbcd9e9a50e
IT25103821 <IT25103821@my.sliit.lk>
2026-05-22 14:42:56 +0530

    Update 1 Doctor.java

--- src/main/java/com/hospital/models/Doctor.java
+++ src/main/java/com/hospital/models/Doctor.java
@@ -18,5 +18,5 @@ public class Doctor {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
     public String getSpecialization() { return specialization; }
-     }
+    public void setSpecialization(String specialization) { this.specialization = specialization; }
 }
