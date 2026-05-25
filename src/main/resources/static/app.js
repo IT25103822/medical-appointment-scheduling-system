6ffc1050eae3f5184aed9a8d5d0661345c4bea98
IT25103822 <it25103822@my.sliit.lk>
2026-05-25 12:11:36 +0530

    Update app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -4,12 +4,12 @@ async function loadPatientData() {
         const response = await fetch('http://localhost:8080/api/patients/info');
         const data = await response.json();
 
-        // UI එක update කරනවා
+        // UI update 
         document.getElementById('p-id').innerText = data.id;
         document.getElementById('p-name').innerText = data.name;
         document.getElementById('p-history').innerText = data.medicalHistory;
 
-        // Info box එක පේන්න සලස්වනවා
+        // Info box 
         document.getElementById('patient-info').style.display = 'block';
     } catch (error) {
         console.error("Error fetching data:", error);
