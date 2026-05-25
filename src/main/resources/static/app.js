e24409263514dc81d1ea4981bd3fd94764e789ff
IT25103822 <it25103822@my.sliit.lk>
2026-05-25 12:11:53 +0530

    Update app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,6 @@
 async function loadPatientData() {
     try {
-        // Backend API endpoint eka call කරනවා
+        // Backend API endpoint eka call 
         const response = await fetch('http://localhost:8080/api/patients/info');
         const data = await response.json();
 
